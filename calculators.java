41d0c3448394a4cb65635384bc75838e7c9045f8
utkarsh136 <81348131+utkarsh136@users.noreply.github.com>
2022-07-19 15:57:21 +0530

    Creating an Arithmetic Calculator.
    The Source Code of Arithmetic calculator to perform add, subtract, multiply, divide, power and modulus.

---
+++ calculators.java
@@ -0,0 +1,67 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class calculators {
+
+	public static void main(String[] args) {
+		
+		char operation;
+		double firstnumber;
+		double secondnumber;
+		double result=0.0;
+		
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Choose a operation:1+, 2-, 3* 4/, 5%, 6^ "  );
+		operation=sc.next().charAt(0);
+		
+		System.out.print("Enter the  first number");
+		firstnumber=sc.nextDouble();
+		System.out.print("Enter the  second number");
+		secondnumber=sc.nextDouble();
+		switch(operation) {
+		case '+':
+			result=firstnumber+secondnumber;
+			System.out.println(firstnumber + " + " + secondnumber + " = " + result);
+			break;
+		case '-':
+			result=firstnumber-secondnumber;
+			System.out.println(firstnumber + " - " + secondnumber + " = " + result);
+			break;
+		case '*':
+			result=firstnumber*secondnumber;
+			System.out.println(firstnumber + " * " + secondnumber + " = " + result);
+			break;
+		case '/':
+			result=firstnumber/secondnumber;
+			System.out.println(firstnumber + " / " + secondnumber + " = " + result);
+			break;
+		case '%':
+			result=firstnumber%secondnumber;
+			System.out.println(firstnumber + " % " + secondnumber + " = " + result);
+			break;
+		
+		case '^':
+			result=Math.pow(firstnumber, secondnumber);
+			System.out.println(firstnumber + " ^ " + secondnumber + " = " + result);
+			break;
+	
+		
+	
+	
+		default:
+			System.out.println("Invalid operation!");
+			break;
+		
+		}
+		
+		sc.close(); 
+		
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
