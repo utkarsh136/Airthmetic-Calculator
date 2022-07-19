7b72284719e1e113d83092acd1455593b0ecd374
utkarsh136 <81348131+utkarsh136@users.noreply.github.com>
2022-07-19 16:26:57 +0530

    Add files via upload

---
+++ stringpractise.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class stringpractise {
+
+	public  static void main(String[] args) {
+		System.out.println("hello");
+		
+		
+		/*for(int i=0;i<=10;i++) {
+			arr[i]=sc.nextInt();
+		}
+		for(int i=0;i<=10;i++) {
+			System.out.println(arr[i]);
+		}*/
+		 
+		Scanner sc=new Scanner(System.in);
+		String i=sc.nextLine();
+		System.out.println(i);
+		String u=
+		
+		
+		
+		
+		
+		
+	
+		String s="abc";
+		String g="home lander";
+		String l=g.toUpperCase();
+		System.out.println(l);
+		int p=l.length();
+		System.out.println(p);
+	
+		sc.close(); 
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
